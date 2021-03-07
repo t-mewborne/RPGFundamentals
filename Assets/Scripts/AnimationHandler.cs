8405df2b2a0ed3130c4705b21b41eedc36148c0b
Travis Mewborne <tmewborne@hotmail.com>
2021-03-07 15:47:50 -0600

    click to move and walking animations working

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isMoving = agent.velocity.magnitude>0;
+        animator.SetBool("isMoving",isMoving);
+    }
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof (NavMeshAgent))]
+public class PlayerControls : MonoBehaviour
+{
+    RaycastHit hitInfo  = new RaycastHit();
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin,ray.direction,out hitInfo)) {
+                agent.destination=hitInfo.point;
+            }
+        }
+    }
+}
