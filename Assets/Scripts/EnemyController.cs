3a6bbea739187dff21781271bd929b148b592e30
Travis Mewborne <tmewborne@hotmail.com>
2021-03-07 17:37:24 -0600

    Added patroling enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof (NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+
+    bool isMoving = false;
+    NavMeshAgent agent;
+    public GameObject player;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position,player.transform.position) < 5) agent.destination = player.transform.position;
+    }
+}
